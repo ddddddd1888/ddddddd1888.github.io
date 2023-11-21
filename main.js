223997b409eb0878b8514b1639c7fbb641f38bac
ddddddd1888 <79388006+ddddddd1888@users.noreply.github.com>
2023-11-20 21:27:05 -0500

    globe

---
+++ main.js
@@ -0,0 +1,68 @@
+import * as THREE from 'three';
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+
+import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
+import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
+
+// console.log(vertexShader)
+// console.log(fragmentShader)
+
+const scene = new THREE.Scene()
+const camera = new THREE.
+    PerspectiveCamera(
+    75,
+    innerWidth / innerHeight,
+    0.1,
+    1000
+)
+
+const renderer = new THREE.WebGLRenderer(
+    {
+    antialias: true
+    }
+);
+renderer.setSize( window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild( renderer.domElement );
+
+// create a sphere
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(5, 50, 50),
+    new THREE.ShaderMaterial({
+      vertexShader,
+      fragmentShader,
+      uniforms: {
+        globeTexture: {
+          value: new THREE.TextureLoader().load('./globe.jpeg')
+        }
+      }
+    })
+  )
+
+
+scene.add(sphere)
+
+// create atmosphere
+const atmosphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.ShaderMaterial({
+    vertexShader: atmosphereVertexShader,
+    fragmentShader: atmosphereFragmentShader,
+    blending: THREE.AdditiveBlending,
+    side: THREE.BackSide
+  })
+)
+
+atmosphere.scale.set(1.1, 1.1, 1.1)
+
+scene.add(atmosphere)
+
+camera.position.z = 20
+
+function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+    sphere.rotation.y += 0.001
+}
+animate()
